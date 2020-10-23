a5f2945dcf93e48f8c9310f81bc1d91e06375e82
Ederson <edersondeveloper@gmail.com>
2020-10-22 20:30:00 -0400

    adding basic files

---
+++ README.md
@@ -0,0 +1,33 @@
+# Deninho Bot (Discord)
+
+Deninho Bot é um bot para discord feito especialmente para o servidor **Caverna do Patocornio**.
+
+# Comandos
+
+## !eita
+
+Conta e mostra quantas vezes a [Levxyca](https://github.com/levxyca) falou eita.
+
+## !caraio
+
+Conta e mostra quantas vezes a [Pachi Codes](https://github.com/pachicodes) falou caraio.
+
+## !alfa
+
+Conta e mostra quantas vezes a [Pachi Codes](https://github.com/pachicodes) falou caraio.
+
+## !splash
+
+responde com `Splash Splash`
+
+## Bom Dia
+
+Caso alguém mande `bom dia` em algum chat, o Deninho irá reponder `Bom Dia!`
+
+## Boa Noite
+
+Caso alguém mande `boa noite` em algum chat, o Deninho irá reponder `Boa Noite!`
+
+## Boas Festas
+
+Caso alguém mande `boas festas` em algum chat, o Deninho irá reponder `Boas Festas!`
---
+++ index.js
@@ -0,0 +1,52 @@
+require('dotenv').config();
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+const prefix = '!'
+
+let eitaCounter = 0
+let caraioCounter = 0
+let panificadoraAlfaCounter = 0
+
+client.on('ready', () => {
+  console.log(`Logged as ${client.user.tag}`);
+});
+
+client.on('message', (message) => {
+  if (message.author.bot) return;
+
+  message.content = message.content.toLowerCase()
+
+  const commandBody = message.content.slice(prefix.length);
+  const args = commandBody.split(' ');
+  const command = args.shift().toLowerCase();
+
+  console.log(message.content)
+
+  if (message.content == 'bom dia' || message.content == 'dia'){
+    message.reply(`Bom Dia!`)
+  }
+  else if (message.content == 'boa noite' || message.content == 'noite'){
+    message.reply(`Boa Noite!`)
+  }
+  else if (message.content == 'boas festas'){
+    message.reply(`Boas Festas!`)
+  }
+  else if (command == 'eita'){
+    eitaCounter++
+    message.reply(`A Lexyca já falou eita ${eitaCounter} vezes`)
+  }
+  else if (command == 'caraio'){
+    caraioCounter++
+    message.reply(`A Pachi já falou caraio ${caraioCounter} vezes`)
+  }
+  else if (command == 'alfa'){
+    panificadoraAlfaCounter++
+    message.reply(`Já escutamos Panificadora Alfa ${panificadoraAlfaCounter} vezes`)
+  }
+  else if (command == 'splash'){
+    message.reply('Splash Splash')
+  }
+});
+
+client.login(process.env.TOKEN);
